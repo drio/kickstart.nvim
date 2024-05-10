ead69b208c19abcd60b829ce4a0c162f9eddb22f
David Rio Deiros <driodeiros@gmail.com>
2024-05-10 07:59:43 -0400

    comment

--- lua/custom/drio/init.lua
+++ lua/custom/drio/init.lua
@@ -38,6 +38,7 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('lazyvim_' .. name, { clear = true })
 end
 
+-- TODO: allow running this headless (see mason updates)
 vim.api.nvim_create_autocmd('VimEnter', {
   group = augroup 'autoupdate',
   callback = function()
